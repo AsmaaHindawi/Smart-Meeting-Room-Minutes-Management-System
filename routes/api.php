87b0c7265e7304174d2e0b2521c3369e1c753b92
SallyAlMasri <sallyalmasri200@gmail.com>
2025-06-25 16:01:29 +0300

    breeze

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\MeetingController;
-use App\Http\Controllers\MinutesOfMeetingController;
-use App\Http\Controllers\NotificationController;
 
-// ✅ Laravel Breeze Auth Routes
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('users', UserController::class);
-
-Route::get('/rooms', [App\Http\Controllers\RoomController::class, 'index']);
-Route::get('/rooms/{id}', [App\Http\Controllers\RoomController::class, 'show']);
-Route::post('/rooms', [App\Http\Controllers\RoomController::class, 'store']);
-Route::put('/rooms/{id}', [App\Http\Controllers\RoomController::class, 'update']);
-Route::delete('/rooms/{id}', [App\Http\Controllers\RoomController::class, 'destroy']);
-
-
-Route::apiResource('meetings', MeetingController::class);
-Route::apiResource('minutes', MinutesOfMeetingController::class);
-
-Route::apiResource('notifications', NotificationController::class);
